87522e2ed3f198a914f4bd43b2cb4b66e0b3de84
edutucci <edutucci@hotmail.com>
2017-03-08 13:33:16 -0300

    added routes

--- controllers/default.js
+++ controllers/default.js
@@ -2,9 +2,27 @@ exports.install = function() {
 	F.route('/', view_index);
 	// or
 	// F.route('/');
+	F.route('/tools', view_tools);
+	F.route('/about', view_about);
+	F.route('/vuecomp', view_components);
 };
 
 function view_index() {
 	var self = this;
 	self.view('index');
+}
+
+function view_tools(){
+	var self = this;
+	self.view('tools');
+}
+
+function view_about(){
+	var self = this;
+	self.view('about');
+}
+
+function view_components(){
+	var self = this;
+	self.view('vuecomponents');
 }
